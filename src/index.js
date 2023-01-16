5c0f610cebf93d26f818a800f38d09a02d6f5f2f
Lubava7 <joheunsaram@mail.ru>
2023-01-17 00:09:04 +0400

    gh-pages

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import BrowserRouter from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+    <App />
+    {/* </React.StrictMode> */}
   </BrowserRouter>,
 
   document.getElementById("root")
